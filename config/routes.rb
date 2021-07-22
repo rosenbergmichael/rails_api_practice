72a03fe9603089b4159e45732ce469ec987974bf
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-07-22 11:03:25 -0400

    create index.html.erb file in views/example folder

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
 
-  get '/examples', to: 'examples#index'
+  get '/examples', to: 'example#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
